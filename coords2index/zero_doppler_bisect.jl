934eec65422c848fb4d7c0d76d6b8103e1187996
Eigil Yuichi Hyldgaard Lippert <eigil.lippert@gmail.com>
2019-09-08 13:34:54 +0200

    baseline zero doppler code, missing comments

--- coords2index/zero_doppler_bisect.jl
+++ coords2index/zero_doppler_bisect.jl
@@ -1,43 +1,54 @@
 """
-    ellipsoid2xyz(lat::Array{Number,N}, lon::Array{Number,N},
-                  height::Array{Number,N}; semi_major_axis=6378137.,
-                  flattening=1/298.257223563)
+    zero_doppler_bisect()
 
-Convert ellipsoid coordinates to cartisian coordinates, based on ellipsoidal
-latitudes (rad), longitudes (rad) and heights (m).
+
 
 # Examples:
 ```jldoctest
-julia> a = 10; e2 = 2; h = [.2, .1, .6];
-julia> lat = [pi/4, 3*pi, pi/6]; lon = [pi/2, pi/4, pi/8];
-julia> x, y, z = ellipsoid2xyz(lat, lon, h, a, e2);
-julia> x
-3-element Array{Float64,1}:
-  2.90566636126016e-8
- -7.14177848998413
- 11.795229079383331
-julia> y
-3-element Array{Float64,1}:
-  4.7453132826267916e8
- -7.141778489984129
-  4.885743855978092
-julia> z
-3-element Array{Float64,1}:
- -4.7453132797983634e8
- -3.637200993467639e-15
- -6.771067811865474
 ```
 """
-function ellipsoid2xyz(lat, lon, height; semi_major_axis=6378137.,
-                       flattening=1/298.257223563)
+function zero_doppler_bisect()
     #=
     TO-DO
-    specify arugment types for lat, lon, height.
+    Change while loop to more advanced optimization
     =#
-    e2 = flattening * (2 - flattening)
-    v = semi_major_axis./sqrt.(1 .-e2*sin.(lat).*sin.(lat))
-    x=(v+height).*cos.(lat).*cos.(lon)
-    y=(v+height).*cos.(lat).*sin.(lon)
-    z=(v.*(1-e2)+height).*sin.(lat)
-    return x, y, z
+
+    #= Define interval for zero Doppler time search, with
+    an interval larger than the data extend for sensitivity
+    at the edges =#
+    data_duration = azimuth_stop_time - azimuth_start_time
+    search_interval_start = azimuth_start_time - data_duration
+    search_interval_end = azimuth_stop_time + data_duration
+
+    # Initial bisection parameters, start iteration with entire data interval
+    search_interval_duration = search_interval_end - search_interval_start
+    small_time_interval = 1e-6
+    max_iter = 100
+    iter = 0
+
+    while (search_interval_duration > small_time_interval) && (iter < max_iter)
+        search_interval_duration = (search_interval_end - search_interval_start) / 2
+        trial_time = search_interval_start + search_interval_duration
+
+        trial_sat_position = polyvalSV(sat_xyz_polynomial, trial_time)
+
+        line_of_sight = point_xyz - trial_sat_position
+
+        # Compute the squint angle
+        trial_sat_velocity = polyvalSV(sat_vel_polynomial, trial_time)
+        sin_squint_angle = los * trial_sat_velocity
+
+        if (sin_squint_angle < 0)
+            search_interval_end = trial_time
+        else
+            search_interval_start = trial_time
+        end
+        iter += 1;
+    end
+
+    if iter == max_iter
+        print("Error, max iteration reached")
+    end
+
+    return [norm(line_of_sight), trial_time]
 end
