051337167a642a62cbbf5a29e546c3c8556124ea
Simon <simon.kok@live.dk>
2019-12-30 11:48:37 +0100

    Deramping updated and tested

--- Work/spectogram_test.m
+++ Work/spectogram_test.m
@@ -1,4 +1,4 @@
-clear all; clc;
+close all; clear all; clc;
 data1 = csvread("real_original.csv");
 data2 = csvread("imag_original.csv");
 data_original = data1 + 1i*data2;
@@ -6,6 +6,10 @@ data_original = data1 + 1i*data2;
 data1 = csvread("real_deramped.csv");
 data2 = csvread("imag_deramped.csv");
 data_deramped = data1 + 1i*data2;
+
+data1 = csvread("real_resample.csv");
+data2 = csvread("imag_resample.csv");
+data_resample = data1 + 1i*data2;
 
 data1 = csvread("real_reramped.csv");
 data2 = csvread("imag_reramped.csv");
@@ -18,10 +22,11 @@ plotXlabel = 'Burst time [s]';
 plotYlabel = 'Frequency [cycles]';
 freqDuration = 1;
 timeDuration = 3;
-
+%%
 plotSpectrogram(data_original, plotTitle, plotXlabel, plotYlabel, caxisMinMax, numSegments, freqDuration, timeDuration)
-
+%%
 plotSpectrogram(data_deramped, plotTitle, plotXlabel, plotYlabel, caxisMinMax, numSegments, freqDuration, timeDuration)
-
+%%
+plotSpectrogram(data_resample, plotTitle, plotXlabel, plotYlabel, caxisMinMax, numSegments, freqDuration, timeDuration)
+%%
 plotSpectrogram(data_reramped, plotTitle, plotXlabel, plotYlabel, caxisMinMax, numSegments, freqDuration, timeDuration)
-
